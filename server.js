52ac805c5ed8db1692bfac825099000af3354662
mossygoldcoin <mossygoldcoin@gmail.com>
2023-12-26 08:13:02 +0900

    server init

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-console.log("test");
+app.use(express.static(path.join(__dirname, "src")));
+
+const port = process.env.PORT || 3000;
+http.listen(port, function () {
+    console.log(`server on!: http://localhost:${port}`);
+});
