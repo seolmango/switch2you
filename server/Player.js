c85adef2090ce446c3d9c906a1d7dc24d99d9656
mossygoldcoin <mossygoldcoin@gmail.com>
2023-12-26 17:12:36 +0900

    이상한거 취소

--- server/Player.js
+++ server/Player.js
@@ -1,7 +1,7 @@
 class Player {
     static MaxCount = 0;
     static #_Count = 0;
-    static #_Id = 0;
+    static #_Id = 1;
     static Instances = {};
 
     static get Count() {
@@ -19,8 +19,10 @@ class Player {
         Player.Instances[this.id] = this;
         Player.#_Count++;
 
-        this.socketId;
-        this.name;
+        this.socketId; // 유저의 소켓 id
+        this.name; // 유저 이름
+        this.room = null; // 참가한 방 객체
+        this.role; // 참가한 방에서의 역할. owner/manager/user
     }
 
     get id() {
@@ -30,6 +32,39 @@ class Player {
     delete() {
         delete Player.Instances[this.id];
         Player.#_Count--;
+    }
+
+    // 방 참가
+    joinRoom(room, password = false) {
+        if (this.room) return 'already join room';
+        if (room.player.length > 8) return 'room full';
+        if (room.password !== password) return 'wrong password';
+
+        this.room = room;
+        this.role = 'user';
+        room.players.push(this);
+    }
+
+    // 플레이어에게 방장 부여
+    getOwner() {
+        if (this.room.owner) this.room.owner.role = 'manager';
+        this.role = 'owner';
+        this.room.owner = this;
+    }
+
+    // 방 퇴장
+    exitRoom() {
+        if (!this.room) return 'no join room';
+        delete this.room.players[this.id];
+
+        ownerChange = false; // 방장이 변경됐는가? (추가 emit을 위해 필요함)
+        if (this.room.player.length < 1) Room.delete(); // 참가 인원 없을시 방 삭제
+        else if (this.role === 'owner') {
+            Object.values(this.room.players)[0].getOwner();
+            ownerChange = this.room.owner;
+        }
+        this.room = null;
+        return ownerChange;
     }
 }
 
