25cb8e2f7735ef3329fc4dfb9edc69a14bf4e4f8
Chaehwan Seol <seolchaehwan@naver.com>
2023-12-26 23:26:37 +0900

    폰트 추가

--- src/Screens/title-screen.js
+++ src/Screens/title-screen.js
@@ -35,17 +35,17 @@ titleScreen.draw = function (Background_ctx, UI_ctx, Screen) {
     UI_ctx.clearRect(0,0,1920,1080);
     if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 960, 540, 300, 100)) {
         drawRoundBox(UI_ctx, 960, 540, 300*1.05, 100*1.05, Color_list.button_blue_2_hex, Color_list.button_blue_3_hex, 10, 5);
-        drawText(UI_ctx,960, 540, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "start game", "center", "");
+        drawText(UI_ctx,960, 540, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "start game", "center", 'GmarketSansMedium');
     } else {
         drawRoundBox(UI_ctx, 960, 540, 300, 100, Color_list.button_blue_1_hex, Color_list.button_blue_2_hex, 10, 5);
-        drawText(UI_ctx,960, 540, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "start game", "center", "");
+        drawText(UI_ctx,960, 540, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "start game", "center", 'GmarketSansMedium');
     }
     if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 960, 700, 300, 100)) {
         drawRoundBox(UI_ctx, 960, 700, 300*1.05, 100*1.05, Color_list.button_blue_2_hex, Color_list.button_blue_3_hex, 10, 5);
-        drawText(UI_ctx,960, 700, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "setting", "center", "");
+        drawText(UI_ctx,960, 700, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "setting", "center", 'GmarketSansMedium');
     } else {
         drawRoundBox(UI_ctx, 960, 700, 300, 100, Color_list.button_blue_1_hex, Color_list.button_blue_2_hex, 10, 5);
-        drawText(UI_ctx,960, 700, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "setting", "center", "");
+        drawText(UI_ctx,960, 700, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "setting", "center", 'GmarketSansMedium');
     }
 };
 
