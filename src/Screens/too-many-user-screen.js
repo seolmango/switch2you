493b4f66aac4502a571000213decc02177d1a5f1
Chaehwan Seol <seolchaehwan@naver.com>
2023-12-30 00:58:26 +0900

    디테일 수정

--- src/Screens/too-many-user-screen.js
+++ src/Screens/too-many-user-screen.js
@@ -15,7 +15,7 @@ tooManyUserScreen.draw = function (Background_ctx, UI_ctx, Screen) {
     drawRoundBox(UI_ctx, 960, 540, 1600, 900, Color_list.button_gray_1_hex, Color_list.button_gray_2_hex, 20, 50);
     drawText(UI_ctx, 960, 200, 70, 0, Color_list.text_default_hex, undefined, undefined, "Server Overloaded!", "center", 'GmarketSansMedium');
     drawText(UI_ctx, 960, 400, 50, 0, Color_list.text_default_hex, undefined, undefined, "There are too many users on our 'tiny little' server.", "center", 'GmarketSansMedium');
-    drawText(UI_ctx, 960, 500, 50, 0, Color_list.text_default_hex, undefined, undefined, "Wait for the user to leave.", "center", 'GmarketSansMedium');
+    drawText(UI_ctx, 960, 500, 50, 0, Color_list.text_default_hex, undefined, undefined, "Wait for other users to leave.", "center", 'GmarketSansMedium');
     drawText(UI_ctx, 960, 600, 50, 0, Color_list.text_default_hex, undefined, undefined, "Please refresh and try connecting again.", "center", 'GmarketSansMedium');
     UI_ctx.drawImage(image.server_overloaded, 810, 675, 300, 300);
 };
