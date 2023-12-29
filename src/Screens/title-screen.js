cdc4a12f40aff6734c7706333898c8e824b60c2e
Chaehwan Seol <seolchaehwan@naver.com>
2023-12-29 22:42:10 +0900

    타이틀 화면 제작

--- src/Screens/title-screen.js
+++ src/Screens/title-screen.js
@@ -10,43 +10,78 @@ titleScreen.initialize = function (Background_ctx, UI_ctx, Screen) {
     UI_ctx.clearRect(0,0,1920,1080);
     titleScreen.checkUIList = [];
     titleScreen.checkUIList.push({
-        tag: "start game",
+        tag: "start-game-title",
         center_x: 960,
-        center_y: 540,
-        width: 300,
-        height: 100,
-        clicked: function (){
-            console.log("start game");
+        center_y: 600,
+        width: 720,
+        height: 120,
+        clicked: function () {
+            console.log("start-game-title")
         }
     });
     titleScreen.checkUIList.push({
-        tag: "setting",
+        tag: "settings-title",
         center_x: 960,
-        center_y: 700,
-        width: 300,
-        height: 100,
-        clicked: function (){
-            console.log("setting");
+        center_y: 840,
+        width: 720,
+        height: 120,
+        clicked: function () {
+            console.log("settings-title")
+        }
+    });
+    titleScreen.checkUIList.push({
+        tag: "help-title",
+        center_x: 1836,
+        center_y: 1008,
+        width: 120,
+        height: 120,
+        clicked: function () {
+            console.log("help-title")
+        }
+    });
+    titleScreen.checkUIList.push({
+        tag: "copy-title",
+        center_x: 1692,
+        center_y: 1008,
+        width: 120,
+        height: 120,
+        clicked: function () {
+            console.log("copy-title")
         }
     });
 };
 
 titleScreen.draw = function (Background_ctx, UI_ctx, Screen) {
     UI_ctx.clearRect(0,0,1920,1080);
-    if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 960, 540, 300, 100)) {
-        drawRoundBox(UI_ctx, 960, 540, 300*1.05, 100*1.05, Color_list.button_blue_2_hex, Color_list.button_blue_3_hex, 10, 5);
-        drawText(UI_ctx,960, 540, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "start game", "center", 'GmarketSansMedium');
-    } else {
-        drawRoundBox(UI_ctx, 960, 540, 300, 100, Color_list.button_blue_1_hex, Color_list.button_blue_2_hex, 10, 5);
-        drawText(UI_ctx,960, 540, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "start game", "center", 'GmarketSansMedium');
+    if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 960, 600, 720, 120)) {
+        drawRoundBox(UI_ctx, 960, 600, 720*1.05, 120*1.05, Color_list.button_blue_2_hex, Color_list.button_blue_3_hex, 10*1.05, 25*1.05);
+        drawText(UI_ctx, 960, 600, 60*1.05, 0, Color_list.text_onmouse_hex, undefined, undefined, "Start Game", "center", "GmarketSansMedium");
+    }else{
+        drawRoundBox(UI_ctx, 960, 600, 720, 120, Color_list.button_blue_1_hex, Color_list.button_blue_2_hex, 10, 25);
+        drawText(UI_ctx, 960, 600, 60, 0, Color_list.text_default_hex, undefined, undefined, "Start Game", "center", "GmarketSansMedium");
     }
-    if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 960, 700, 300, 100)) {
-        drawRoundBox(UI_ctx, 960, 700, 300*1.05, 100*1.05, Color_list.button_blue_2_hex, Color_list.button_blue_3_hex, 10, 5);
-        drawText(UI_ctx,960, 700, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "setting", "center", 'GmarketSansMedium');
-    } else {
-        drawRoundBox(UI_ctx, 960, 700, 300, 100, Color_list.button_blue_1_hex, Color_list.button_blue_2_hex, 10, 5);
-        drawText(UI_ctx,960, 700, 50, 0, Color_list.button_gray_3_hex, undefined, undefined, "setting", "center", 'GmarketSansMedium');
+    if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 960, 840, 720, 120)){
+        drawRoundBox(UI_ctx, 960, 840, 720*1.05, 120*1.05, Color_list.button_red_2_hex, Color_list.button_red_3_hex, 10*1.05, 25*1.05);
+        drawText(UI_ctx, 960, 840, 60*1.05, 0, Color_list.text_onmouse_hex, undefined, undefined, "Settings", "center", "GmarketSansMedium");
+    }else{
+        drawRoundBox(UI_ctx, 960, 840, 720, 120, Color_list.button_red_1_hex, Color_list.button_red_2_hex, 10, 25);
+        drawText(UI_ctx, 960, 840, 60, 0, Color_list.text_default_hex, undefined, undefined, "Settings", "center", "GmarketSansMedium");
     }
+    if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 1836, 1008, 120,120)){
+        drawRoundBox(UI_ctx, 1836, 1008, 120*1.05, 120*1.05, Color_list.button_gray_2_hex, Color_list.button_gray_3_hex, 10*1.05, 25*1.05);
+        drawText(UI_ctx, 1836, 1008, 60*1.05, 0, Color_list.text_onmouse_hex, undefined, undefined, "?", "center", "GmarketSansMedium");
+    }else{
+        drawRoundBox(UI_ctx, 1836, 1008, 120, 120, Color_list.button_gray_1_hex, Color_list.button_gray_2_hex, 10, 25);
+        drawText(UI_ctx, 1836, 1008, 60, 0, Color_list.text_default_hex, undefined, undefined, "?", "center", "GmarketSansMedium");
+    }
+    if(checkTouch(Screen.userMouse.x, Screen.userMouse.y, 1692, 1008, 120, 120)){
+        drawRoundBox(UI_ctx, 1692, 1008, 120*1.05, 120*1.05, Color_list.button_gray_2_hex, Color_list.button_gray_3_hex, 10*1.05, 25*1.05);
+        drawText(UI_ctx, 1692, 1008, 60*1.05, 0, Color_list.text_onmouse_hex, undefined, undefined, "ⓒ", "center", "GmarketSansMedium");
+    }else{
+        drawRoundBox(UI_ctx, 1692, 1008, 120, 120, Color_list.button_gray_1_hex, Color_list.button_gray_2_hex, 10, 25);
+        drawText(UI_ctx, 1692, 1008, 60, 0, Color_list.text_default_hex, undefined, undefined, "ⓒ", "center", "GmarketSansMedium");
+    }
+    drawText(UI_ctx, 10, 1060, 30, 0, Color_list.text_default_hex, undefined, undefined, "swITch-webclient", "left", "GmarketSansMedium");
 };
 
 titleScreen.check = function (userMouse, userKeyboard, checkUIList) {
@@ -62,7 +97,7 @@ titleScreen.check = function (userMouse, userKeyboard, checkUIList) {
 
 titleScreen.redrawBackground = function (Background_ctx) {
     Background_ctx.clearRect(0,0,1920,1080);
-    Background_ctx.drawImage(image.title_logo, 618, 0, 684, 300);
+    Background_ctx.drawImage(image.title_logo, 447, 30, 1026, 450);
 };
 
 export { titleScreen };
