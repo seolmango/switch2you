0168a7d6914d218f1dbf1995631d32ce0f14b7b5
mossygoldcoin <mossygoldcoin@gmail.com>
2024-01-06 15:07:54 +0900

    player id로 잘못돼있던거 전부 number로 수정, number 변경 emit on 추가, 약간의 에러 수정

--- server/Room.js
+++ server/Room.js
@@ -30,11 +30,11 @@ class Room {
         this.players = [owner]; // 참가한 유저 객체들
         this.owner = owner; // 방장인 플레이어 객체. 이 프로퍼티가 굳이 왜 필요하나 싶지만, 방에 owner는 1명 뿐이라는 특이성 때문에 필요함.
         if (roomName) this.name = roomName; // 방 이름
-        else this.name = owner.name + '의 방';
+        else this.name = owner.name + '\'s Room';
         owner.room = this;
         owner.role = 'owner';
         owner.number = 1;
-        this.numbers = [0, 1, 1, 1, 1, 1, 1, 1]; // 안 쓴 번호들
+        this.numbers = [owner.id, 0, 0, 0, 0, 0, 0, 0]; // 번호 안 썼는지 여부. 0이면 안썼고, 0이 아니면 썼음. 0이 아닌 숫자는 그 번호에 해당하는 player의 id.
     }
 
     get id() {
