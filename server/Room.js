526a267e0c32fea0bc6a6f5243b7cfd92090fde7
mossygoldcoin <mossygoldcoin@gmail.com>
2024-01-20 08:37:23 +0900

    코드 비효율적인거 수정

--- server/Room.js
+++ server/Room.js
@@ -27,15 +27,13 @@ class Room {
         this.public = public_; // 공개방인가? 공개방은 퀵매칭 포함됨.
         this.password = password; // 방 비밀번호
         this.playing = false; // 게임중인가? 게임중인방은 퀵매칭에서 제외됨.
-        this.players = [owner]; // 참가한 유저 객체들
+        this.players = []; // 참가한 유저 객체들
         this.owner = owner; // 방장인 플레이어 객체. 이 프로퍼티가 굳이 왜 필요하나 싶지만, 방에 owner는 1명 뿐이라는 특이성 때문에 필요함.
         if (roomName) this.name = roomName; // 방 이름
         else this.name = owner.name + '\'s Room';
-        owner.room = this;
+        this.numbers = [0, 0, 0, 0, 0, 0, 0, 0]; // 번호 안 썼는지 여부. 0이면 안썼고, 0이 아니면 썼음. 0이 아닌 숫자는 그 번호에 해당하는 player의 id.
+        owner.joinRoom(this, password);
         owner.role = 'owner';
-        owner.number = 1;
-        owner.skill = 'dash';
-        this.numbers = [owner.id, 0, 0, 0, 0, 0, 0, 0]; // 번호 안 썼는지 여부. 0이면 안썼고, 0이 아니면 썼음. 0이 아닌 숫자는 그 번호에 해당하는 player의 id.
     }
 
     get id() {
