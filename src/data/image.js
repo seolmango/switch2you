af248ac23937a794850d88d767832744320fa395
Chaehwan Seol <seolchaehwan@naver.com>
2024-01-19 21:57:52 +0900

    이미지 용량을 줄이기 위해 png에서 webp로 변경

--- src/data/image.js
+++ src/data/image.js
@@ -1,8 +1,8 @@
 const image = {}
 image.title_logo = new Image();
-image.title_logo.src = 'image/logo.png';
+image.title_logo.src = 'image/logo.webp';
 image.server_overloaded = new Image();
-image.server_overloaded.src = 'image/server-overload.png';
+image.server_overloaded.src = 'image/server-overload.webp';
 image.credit_seolmango = new Image();
 image.credit_seolmango.src = 'https://avatars.githubusercontent.com/u/65744726?v=4';
 image.credit_mossy = new Image();
@@ -10,11 +10,11 @@ image.credit_mossy.src = 'https://avatars.githubusercontent.com/u/72716691?v=4';
 image.credit_H = new Image();
 image.credit_H.src = 'https://i1.sndcdn.com/avatars-tpEDqrcDB5pf6zbP-owBUSg-t500x500.jpg';
 image.skill_switch = new Image();
-image.skill_switch.src = 'image/asset/switch.png';
+image.skill_switch.src = 'image/asset/switch.webp';
 image.skill_dash = new Image();
-image.skill_dash.src = 'image/asset/dash.png';
+image.skill_dash.src = 'image/asset/dash.webp';
 image.skill_teleport = new Image();
-image.skill_teleport.src = 'image/asset/tele.png';
+image.skill_teleport.src = 'image/asset/tele.webp';
 image.pc_icon = new Image();
 image.pc_icon.src = 'image/asset/pc.svg';
 image.mobile_icon = new Image();
