823bcf7306284d3049a6dde21693ab335bfc29e0
Seolchaehwan <seolchaehwan@naver.com>
2024-02-15 22:53:27 +0900

    JsDoc 적용

--- src/Screens/tools/drawRoundBox.js
+++ src/Screens/tools/drawRoundBox.js
@@ -1,3 +1,15 @@
+/**
+ * Function to draw rounded box on the canvas
+ * @param {CanvasRenderingContext2D} ctx - canvas context object to draw on
+ * @param {number} x - x coordinate of the center of the box
+ * @param {number} y - y coordinate of the center of the box
+ * @param {number} width - width of the box
+ * @param {number} height - height of the box
+ * @param {string | null | undefined} fillColor - inside color of the box
+ * @param {string | null | undefined} strokeColor - color of the stroke
+ * @param {number | null | undefined} strokeWidth - width of the stroke
+ * @param {number} arcRadius - radius of the arc(Roundness of the box)
+ */
 const drawRoundBox = function (ctx, x, y, width, height, fillColor, strokeColor, strokeWidth, arcRadius) {
     ctx.save();
     ctx.beginPath();
