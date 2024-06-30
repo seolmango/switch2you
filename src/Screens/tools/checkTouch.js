0c4ddce731bce827cb8e973098375e15c910a228
Seolchaehwan <seolchaehwan@naver.com>
2024-06-30 11:58:06 +0900

    클릭했는지 확인하는 함수 수정

--- src/Screens/tools/checkTouch.js
+++ src/Screens/tools/checkTouch.js
@@ -1,4 +1,8 @@
-const checkTouch = function (mouseX, mouseY, x, y, width, height) {
+const checkTouch = function (mouseX, mouseY, x, y, width, height, displayDPI) {
+    x = x * displayDPI;
+    y = y * displayDPI;
+    width = width * displayDPI;
+    height = height * displayDPI;
     const deltaX = mouseX - x;
     const deltaY = mouseY - y;
     if (deltaX >= width * -0.5 && deltaX <= width * 0.5 && deltaY >= height * -0.5 && deltaY <= height * 0.5) {
